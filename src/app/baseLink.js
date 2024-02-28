7fd4268bde7d71d12e75042ef23434bfc76ae990
nagdatt.gajjam_CRI-UIH <nagdatt.gajjam@united-imaging.com>
2024-02-28 18:35:08 +0530

    base link changed

--- src/app/baseLink.js
+++ src/app/baseLink.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const backendFunc= () => {
- const  __API__ = "http://localhost:5000/"
+//  const  __API__ = "http://localhost:5000/"
+const  __API__ = "http://13.232.174.92/server/"
+
   return axios.create({baseURL: __API__})
 }
 
